fa19ab33d791a33d25bba373bd283df8c518b1f4
WilerMS <wilersoriano@gmail.com>
2023-06-26 01:32:38 +0200

    client and server sync

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,1 +1,19 @@
-<h1>Hello world</h1>
+<script>
+
+  let texto = ''
+  const get = async () => {
+
+    const API_URL = import.meta.env.VITE_SERVER_HOST
+    const Url = new URL(`http://${API_URL}`)
+    const res = await fetch(Url)
+    const data = await res.text()
+
+    console.log({ data })
+    texto = data
+  }
+
+  get()
+
+</script>
+
+<h1>Response from server: {texto}</h1>
