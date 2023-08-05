f34b4649299d96da8f2147fe4498233c7e6f3c34
WilerMS <wilersoriano@gmail.com>
2023-08-05 07:20:02 +0200

    Add users routes

--- server/src/app.py
+++ server/src/app.py
@@ -1,17 +1,18 @@
 from flask import Flask
 from flask_cors import CORS
-from flask_restful import Api
-from resources.images import ImagesController
-from resources.users import UsersController
+from routes.users import users
+from werkzeug.exceptions import HTTPException
+from flask_expects_json import ValidationError
+from errors.errors import handle_exception, handle_validation_error, handle_db_duplicate_key_error
+from pymongo.errors import PyMongoError, DuplicateKeyError
 
 app = Flask(__name__)
-api = Api(app)
 CORS(app)
 
-api.add_resource(UsersController, '/users', '/users/<string:username>')
-api.add_resource(
-  ImagesController,
-  '/images',
-  '/images/<string:param>',
-  '/images/<string:param>/<string:action>'
-)
+app.register_blueprint(users, url_prefix='/users')
+
+# Registering errors
+app.register_error_handler(400, handle_validation_error)
+app.register_error_handler(DuplicateKeyError, handle_db_duplicate_key_error)
+app.register_error_handler(PyMongoError, handle_exception)
+app.register_error_handler(HTTPException, handle_exception)
---
+++ server/src/routes/users.py
@@ -0,0 +1,45 @@
+from flask import Blueprint, jsonify, request, Response
+from bson import json_util
+from models.Users import UsersModel
+from flask_expects_json import expects_json
+from schemas.users_schema import users_route_schema
+from utils.schema_utils import replace_schema_require_properties
+from werkzeug.exceptions import NotFound
+
+users = Blueprint('users', __name__)
+
+### GET ###
+@users.route('/<string:username>', methods=['GET'])
+def get_user(username: str):
+  user = UsersModel.find_one(username)
+  if not user:
+    raise NotFound("User not found")
+  return Response(json_util.dumps(user), mimetype='application/json')
+
+### POST ###
+@users.route('/', methods=['POST'])
+@expects_json(users_route_schema)
+def create_user():
+  user_data = request.json
+  user = UsersModel.insert_one(user_data)
+  return Response(json_util.dumps(user), mimetype='application/json')
+
+### PUT ###
+@users.route('/<string:id>', methods=['PUT'])
+@expects_json(replace_schema_require_properties(users_route_schema, []))
+def update_user(id: str):
+  user = UsersModel.find_one_by_id(id)
+  if not user:
+    raise NotFound("User not found")
+  user_data = request.json
+  user = UsersModel.update_one(id, user_data)
+  return Response(json_util.dumps(user), mimetype='application/json')
+
+### DELETE ###
+@users.route('/<string:id>', methods=['DELETE'])
+def delete_user(id: str):
+  user = UsersModel.find_one_by_id(id)
+  if not user:
+    raise NotFound("User not found")
+  UsersModel.delete_one(id)
+  return jsonify({ "message": "User Successfully deleted"}), 200
