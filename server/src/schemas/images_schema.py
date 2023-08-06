57d88e4c73878ff62db7551e2e67392d978a4270
WilerMS <wilersoriano@gmail.com>
2023-08-06 23:50:56 +0200

    Add images schema

--- server/src/schemas/images_schema.py
+++ server/src/schemas/images_schema.py
@@ -1,29 +1,33 @@
-images_schema = {
-  "$jsonSchema": {
-    "bsonType": "object",
-    "additionalProperties": False,
-    "required": ["title", "url", "likes", "userId"],
-    "properties": {
-      "title": {
-        "bsonType": "string"
-      },
-      "description": {
-        "bsonType": "string"
-      },
-      "url": {
-        "bsonType": "string"
-      },
-      "private": {
-        "bsonType": "boolean",
-        "default": False
-      },
-      "likes": {
-        "bsonType": "number",
-        "default": 0
-      },
-      "userId": {
-        "bsonType": "string"
-      }
+from utils.schema_utils import transform_schema_to_mongo_schema
+
+images_route_schema = {
+  "type": "object",
+  "additionalProperties": False,
+  "required": ["title", "url", "likes", "userId"],
+  "properties": {
+    "title": {
+      "type": "string"
+    },
+    "description": {
+      "type": "string"
+    },
+    "url": {
+      "type": "string"
+    },
+    "private": {
+      "type": "boolean",
+      "default": False
+    },
+    "likes": {
+      "type": "number",
+      "default": 0
+    },
+    "userId": {
+      "type": "string"
     }
   }
+}
+
+images_db_schema = {
+  "$jsonSchema": transform_schema_to_mongo_schema(images_route_schema)
 }
