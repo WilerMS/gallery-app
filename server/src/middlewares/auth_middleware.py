98ffaa82d1726b9db9bea48f468a6d2ff9a54ca6
WilerMS <wilersoriano@gmail.com>
2023-08-07 06:14:06 +0200

    modify auth_middleware to be able to allow unauthenticated users

--- server/src/middlewares/auth_middleware.py
+++ server/src/middlewares/auth_middleware.py
@@ -5,24 +5,31 @@ from constants.env import JWT_SECRET_KEY
 from models.UsersModel import UsersModel
 import jwt
 
-def auth_middleware(function: Callable):
-  def decorated(*args, **kwargs):
-    token = None
-    if "Authorization" in request.headers:
-      token = request.headers["Authorization"].split(" ")[1]
+def auth_middleware(allow_unauthenticated: bool = False):
+  def decorator(function: Callable):
+    def decorated(*args, **kwargs):
 
-    if not token:
-      raise Unauthorized("Provided token is not valid")
-    
-    data = jwt.decode(token, JWT_SECRET_KEY, algorithms=["HS256"])
-    username = data['username']
+      token = None
+      if "Authorization" in request.headers:
+        token = request.headers["Authorization"].split(" ")[1]
+      
+      # If decorator allow unauthenticated users
+      if not token and allow_unauthenticated:
+        return function(current_user=None, *args, **kwargs)
 
-    user = UsersModel.find_one(username)
+      if not token:
+        raise Unauthorized("Provided token is not valid")
+      
+      data = jwt.decode(token, JWT_SECRET_KEY, algorithms=["HS256"])
+      username = data['username']
 
-    if not user:
-      raise Unauthorized("Provided token is not valid")
-    
-    current_user = user
-    return function(current_user, *args, **kwargs)
+      user = UsersModel.find_one(username)
+
+      if not user:
+        raise Unauthorized("Provided token is not valid")
+      
+      current_user = user
+      return function(current_user, *args, **kwargs)
+    return decorated
   
-  return decorated
+  return decorator
