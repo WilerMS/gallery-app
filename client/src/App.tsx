ba143d9857966822fef2aac8a196105109eef616
WilerMS <wilersoriano@gmail.com>
2023-08-13 23:07:12 +0200

    Add lazy loading in routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,31 @@
+import { lazy, Suspense } from 'react'
 import {
   BrowserRouter as Router,
   Route,
   Routes
 } from 'react-router-dom'
-import Home from '@pages/Home'
-import NotFound from '@pages/NotFound'
-import Explore from '@pages/Explore'
 import Header from '@components/Header'
+
+const Home = lazy(() => import('@pages/Home'))
+const Explore = lazy(() => import('@pages/Explore'))
+const Profile = lazy(() => import('@pages/Profile'))
+const Settings = lazy(() => import('@pages/Settings'))
+const NotFound = lazy(() => import('@pages/NotFound'))
 
 export default function App () {
   return (
     <div className='App'>
       <Router>
         <Header />
+          <Suspense>
         <Routes>
-          <Route key='home' path='/' element={<Home />} />
-          <Route key='explore' path='/explore' element={<Explore />} />
-          <Route key='notfound' path='*' element={<NotFound />} />
+            <Route key='home' path='/' element={<Home />} />
+            <Route key='explore' path='/explore' element={<Explore />} />
+            <Route key='profile' path='/profile/:userName' element={<Profile />} />
+            <Route key='settings' path='/settings' element={<Settings />} />
+            <Route key='notfound' path='*' element={<NotFound />} />
         </Routes>
+            </Suspense>
       </Router>
     </div>
   )
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import Figure from '@app/components/Figure'
+import { AddIcon } from '@app/icons'
 import { api } from '@app/services/api'
 import { type Image, type FetchImages } from '@app/types'
 import { useEffect, useState } from 'react'
@@ -19,11 +20,16 @@ export default function Home () {
 
   return (
     <main className='px-6 w-full'>
-      <div className="w-full h-full md:columns-8  md:gap-[30px]">
+      <div className="w-full h-full md:columns-4 lg:columns-10 md:gap-5">
         {images.map((image) => (
           <Figure key={image._id.$oid} {...image} />
         ))}
       </div>
+
+      <button className='h-16 w-16 border-2 rounded-full center fixed bottom-6 right-6 shadow-lg bg-white dark:bg-slate-800 dark:border-gray-800'>
+        <AddIcon width={32} height={32}/>
+      </button>
+
     </main>
   )
 }
