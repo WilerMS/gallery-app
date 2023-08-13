eba41e9d02d2255ea90174b51e4da4249ee8fc95
WilerMS <wilersoriano@gmail.com>
2023-08-13 15:41:04 +0200

    Add and setup react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,16 +3,20 @@ import {
   Route,
   Routes
 } from 'react-router-dom'
-import Home from './pages/Home'
-import NotFound from './pages/NotFound'
+import Home from '@pages/Home'
+import NotFound from '@pages/NotFound'
+import Explore from '@pages/Explore'
+import Header from '@components/Header'
 
 export default function App () {
   return (
     <div className='App'>
       <Router>
+        <Header />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='*' element={<NotFound />} />
+          <Route key='home' path='/' element={<Home />} />
+          <Route key='explore' path='/explore' element={<Explore />} />
+          <Route key='notfound' path='*' element={<NotFound />} />
         </Routes>
       </Router>
     </div>
