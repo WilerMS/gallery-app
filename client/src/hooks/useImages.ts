3a66b22b34b41d512bec30a5eb49000a75e4e94e
WilerMS <wilersoriano@gmail.com>
2023-08-15 23:18:24 +0200

    refactor useImages to accept filter changes

--- client/src/hooks/useImages.ts
+++ client/src/hooks/useImages.ts
@@ -9,20 +9,21 @@ export const useImages = () => {
 
   useEffect(() => {
     fetchImages({ page, limit: 20, ...filters })
-      .then((images) => setImages(images))
+      .then((images) => {
+        if (page === 1) {
+          setImages(images)
+        } else if (page > 1) {
+          setImages(prev => [...prev, ...images])
+        }
+      })
       .catch(err => console.log('Error fetching images:', err))
-  }, [])
-
-  useEffect(() => {
-    if (page > 1) {
-      fetchImages({ page, limit: 20, ...filters })
-        .then((images) => setImages(prev => [...prev, ...images]))
-        .catch(err => console.log('Error fetching images:', err))
-    }
-  }, [page])
+  }, [page, filters])
 
   const fetchNextPage = () => setPage(prevPage => prevPage + 1)
-  const modifyFilters = (newFilter: Record<string, string | number>) => setFilters({ ...filters, ...newFilter })
+  const modifyFilters = (newFilter: Record<string, string | number>) => {
+    setFilters({ ...filters, ...newFilter })
+    setPage(1)
+  }
 
   return {
     page,
