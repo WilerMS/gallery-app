be9f33ae9ea9eaee1b92924f8f2b08c562973863
WilerMS <wilersoriano@gmail.com>
2023-08-15 22:43:38 +0200

    Add infinite scroll

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,32 +1,38 @@
 import Figure from '@app/components/Figure'
+import { useImages } from '@app/hooks/useImages'
 import { AddIcon } from '@app/icons'
-import { api } from '@app/services/api'
-import { type Image, type FetchImages } from '@app/types'
-import { useEffect, useState } from 'react'
+import cn from 'classnames'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 export default function Home () {
-  const [images, setImages] = useState<Image[]>([])
-
-  useEffect(() => {
-    api<FetchImages>('http://localhost:5000/api/v1/images?limit=100')
-      .then((data) => {
-        setImages(data.images)
-        console.log(data)
-      })
-      .catch(() => {
-
-      })
-  }, [])
+  const { images, fetchNextPage } = useImages()
 
   return (
     <main className='px-6 w-full'>
-      <div className="w-full h-full md:columns-4 lg:columns-10 md:gap-5">
-        {images.map((image) => (
+      <InfiniteScroll
+        className={cn(
+          'w-full h-full',
+          'md:columns-4 md:gap-5',
+          'lg:columns-6',
+          'xl:columns-8',
+          '2xl:columns-10'
+        )}
+        dataLength={images.length}
+        next={fetchNextPage}
+        hasMore={true}
+        loader={<></>}
+      >
+        {images.map((image, i) => (
           <Figure key={image._id.$oid} {...image} />
         ))}
-      </div>
+      </InfiniteScroll>
 
-      <button className='h-16 w-16 border-2 rounded-full center fixed bottom-6 right-6 shadow-lg bg-white dark:bg-slate-800 dark:border-gray-800'>
+      <button
+        className={cn(
+          'h-16 w-16 border-2 rounded-full center fixed bottom-6 right-6 shadow-lg bg-white',
+          'dark:bg-slate-800 dark:border-gray-800'
+        )}
+      >
         <AddIcon width={32} height={32}/>
       </button>
 
