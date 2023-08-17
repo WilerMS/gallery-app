fc722897d5ed5331be2b60f331eafc2170784e55
WilerMS <wilersoriano@gmail.com>
2023-08-18 01:36:30 +0200

    Add header responsive

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,7 +1,8 @@
-import { Navbar, NavbarContent, NavbarItem, Input, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu, Avatar } from '@nextui-org/react'
+import { Navbar, NavbarContent, NavbarItem, Input, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu, Avatar, NavbarMenu, NavbarMenuItem, NavbarMenuToggle } from '@nextui-org/react'
 import { Link, useLocation } from 'react-router-dom'
 import DarkModeSwitcher from './DarkModeSwitcher'
 import { useTheme } from 'next-themes'
+import { useState } from 'react'
 
 export default function Header () {
   const location = useLocation()
@@ -9,14 +10,20 @@ export default function Header () {
 
   const checkActive = (path: string) => location.pathname === path
   const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light')
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const handleCloseMenu = () => setIsMenuOpen(false)
 
   return (
-    <Navbar maxWidth='full' height='90px'>
+    <Navbar maxWidth='full' height='90px' onMenuOpenChange={setIsMenuOpen}>
       <NavbarContent>
-        <span className="md:text-2xl bg-slate-800 text-white md:px-6 py-2 rounded-2xl mr-8">
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+          className="sm:hidden"
+        />
+        <span className="md:text-2xl bg-slate-800 text-white px-4 md:px-6 py-2 rounded-2xl mr-8">
           La gallerie d&apos;Eve
         </span>
-        <NavbarContent className='gap-6'>
+        <NavbarContent className='gap-6  hidden md:flex'>
           <NavbarItem className='text-lg' isActive={checkActive('/')}>
             <Link to='/'>Home</Link>
           </NavbarItem>
@@ -26,7 +33,7 @@ export default function Header () {
         </NavbarContent>
       </NavbarContent>
 
-      <NavbarContent as="div" justify="end">
+      <NavbarContent as="div" className='!flex-grow-0'>
 
         <Dropdown placement="bottom-end">
           <DropdownTrigger>
@@ -68,6 +75,15 @@ export default function Header () {
           </DropdownMenu>
         </Dropdown>
       </NavbarContent>
+
+      <NavbarMenu>
+        <NavbarMenuItem>
+          <Link to='/'>Home</Link>
+        </NavbarMenuItem>
+        <NavbarMenuItem>
+          <Link to='/explore'>Explore</Link>
+        </NavbarMenuItem>
+      </NavbarMenu>
     </Navbar>
   )
 }
