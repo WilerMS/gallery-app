cfc06e31271b430a33ac8e3329d57d4d3c3679c8
WilerMS <wilersoriano@gmail.com>
2023-08-22 03:27:52 +0200

    add useImages integration with redu toolkit

--- client/src/hooks/useImages.ts
+++ client/src/hooks/useImages.ts
@@ -1,35 +1,43 @@
 import { useEffect, useState } from 'react'
 import { fetchImages } from '@app/services/images'
-import { type Image } from '@app/types'
+import { useAppDispatch, useAppSelector } from '@redux/hooks'
+import { addImages, reset } from '@redux/features/imagesReducer'
+import { type FiltersState, nextFilterPage, setFilters, resetFilters } from '@redux/features/filtersReducer'
 
 export const useImages = () => {
-  const [images, setImages] = useState<Image[]>([])
-  const [page, setPage] = useState(1)
   const [lastPage, setLastPage] = useState(false)
-  const [filters, setFilters] = useState({ })
+  const { images } = useAppSelector(state => state.images)
+  const filters = useAppSelector(state => state.filters)
+
+  const dispatch = useAppDispatch()
 
   useEffect(() => {
-    fetchImages({ page, limit: 20, ...filters })
+    return () => {
+      dispatch(resetFilters())
+    }
+  }, [])
+
+  useEffect(() => {
+    fetchImages({ ...filters })
       .then((data) => {
-        if (page === 1) {
-          setImages(data.images)
+        if (filters.page === 1) {
+          dispatch(reset(data.images))
           setLastPage(data.lastPage)
-        } else if (page > 1) {
-          setImages(prev => [...prev, ...data.images])
+        } else if (filters.page > 1) {
+          dispatch(addImages(data.images))
           setLastPage(data.lastPage)
         }
       })
       .catch(err => console.log('Error fetching images:', err))
-  }, [page, filters])
+  }, [filters.page, filters.q])
 
-  const fetchNextPage = () => setPage(prevPage => prevPage + 1)
-  const modifyFilters = (newFilter: Record<string, string | number>) => {
-    setFilters({ ...filters, ...newFilter })
-    setPage(1)
+  const fetchNextPage = () => dispatch(nextFilterPage())
+  const modifyFilters = (newFilter: FiltersState) => {
+    dispatch(setFilters(newFilter))
   }
 
   return {
-    page,
+    page: filters.page,
     images,
     lastPage,
     fetchNextPage,
