feb6059a9069c6a88a89d939c7dd87b060a19e35
WilerMS <wilersoriano@gmail.com>
2023-08-22 03:19:53 +0200

    add store provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,14 +4,18 @@ import App from './App.tsx'
 import './global.css'
 import { NextUIProvider } from '@nextui-org/react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import { Provider } from 'react-redux'
+import { store } from '@redux/store.ts'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <NextUIProvider>
-      <NextThemesProvider attribute="class" defaultTheme="dark">
-        <App />
-      </NextThemesProvider>
-    </NextUIProvider>
+     <Provider store={store}>
+      <NextUIProvider>
+        <NextThemesProvider attribute="class" defaultTheme="dark">
+          <App />
+        </NextThemesProvider>
+      </NextUIProvider>
+    </Provider>
   </React.StrictMode>
 )
