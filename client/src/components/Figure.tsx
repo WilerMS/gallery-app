2c1d292d51b1511921bb991774d770485d67c4b5
WilerMS <wilersoriano@gmail.com>
2023-08-22 03:20:04 +0200

    Add lazy loading to images

--- client/src/components/Figure.tsx
+++ client/src/components/Figure.tsx
@@ -13,6 +13,7 @@ const Figure: FC<Props> = ({ title, url, likes, liked }) => {
         className="w-full h-full object-cover z-0"
         alt={title}
         src={url}
+        loading='lazy'
       />
       <button className="absolute group-hover:opacity-100 transition-all opacity-0 top-2 right-2 rounded-full bg-white w-[30px] h-[30px] flex items-center justify-center shadow-lg">
         <DeleteIcon />
