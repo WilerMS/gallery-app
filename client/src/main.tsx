ae554ac03f7ef94d1256579f72ebaaaa32f4a69e
WilerMS <wilersoriano@gmail.com>
2023-10-08 01:47:30 +0200

    Add react query to project

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,16 +6,21 @@ import { NextUIProvider } from '@nextui-org/react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { Provider } from 'react-redux'
 import { store } from '@redux/store.ts'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-     <Provider store={store}>
-      <NextUIProvider>
-        <NextThemesProvider attribute="class" defaultTheme="dark">
-          <App />
-        </NextThemesProvider>
-      </NextUIProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <NextUIProvider>
+          <NextThemesProvider attribute="class" defaultTheme="dark">
+            <App />
+          </NextThemesProvider>
+        </NextUIProvider>
+        </QueryClientProvider>
     </Provider>
   </React.StrictMode>
 )
